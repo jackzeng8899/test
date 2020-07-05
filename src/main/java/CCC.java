4c4e0b48342cbea1c6e6f68b17e8726ccf5033b6
zengmin <80494702@qq.com>
2020-07-05 23:25:30 +0800

    zm提交

--- src/main/java/CCC.java
+++ src/main/java/CCC.java
@@ -1,8 +1,7 @@
 public class CCC {
     public static void main(String[] args) {
-        String jj="jjjjjjj";
-        String cc="cccccccccc";
+        String jj="jj1221212jjjjj";
+        String cc="cccc2313113cccccc";
         String dd="dddddd";
-        String ee="dkkkdk";
     }
 }
